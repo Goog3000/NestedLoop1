3eaae75d19753f3e268bde41ca1cd6dde8f76cbb
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-12-02 14:07:05 +0100

    Add project files.

---
+++ NestedLoop1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NestedLoop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("$");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
